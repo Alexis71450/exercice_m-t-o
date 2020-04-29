6378f187b99f03eca83e0931d6bcae5cdffda203
System Administrator <root@Joelles-iMac.local>
2020-04-29 10:05:27 +0200

    Front et css

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<h1>Exercice Météo</h1>
+
+<p>Lien pour récupérer les données météo d'une position via coordonnées GPS(longitude et lattitude):</p>
+<a href="https://www.prevention-meteo.ch/services/json/lat=XXXlng=YYY">https://ww.prevention-meteo.ch/services/json/lat=XXXlng=YYY</a>
+<p>(remplacer XX par lattitude et YYY par la longitude)</p>    
+<p>Pdf explicatif de l'API:</p>
+<a href="https://www.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf">https://ww.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf</a>
+<button class="buttons">Voir la météo sur ma position</button>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
